1c6ac56144a3d277eb6c47673429ce9c15fc87a0
kaguithiajohnson@gmail.com <kaguithiajohnson@gmail.com>
2023-10-20 00:07:34 +0300

    Alter field price on product.

--- product/models.py
+++ product/models.py
@@ -23,7 +23,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField()
     description = models.TextField(blank=True, null=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2) # 9999.99
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='uploads/', blank=True, null=True)
     thumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True)
     date_added = models.DateTimeField(auto_now_add=True)
